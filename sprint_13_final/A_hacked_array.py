ce15bfe799a2c37a1517fd1dc57e266150c81e85
andrew_cherry <chernyaevav@gmail.com>
2022-12-19 21:51:31 +0300

    A_поиск в сломанном массиве

---
+++ sprint_13_final/A_hacked_array.py
@@ -0,0 +1,44 @@
+# ID: 79761153
+
+def broken_search(nums, target) -> int:
+    left = 0
+    right = len(nums) - 1
+    index = -1
+    while left <= right:
+        if nums[left] == target:
+            return left
+        if nums[right] == target:
+            return right
+        mid = (left + right) // 2
+        if nums[mid] == target:
+            return mid
+
+        if nums[left] <= nums[mid]:
+            if nums[left] < target < nums[mid]:
+                right = mid - 1
+            else:
+                left = mid + 1
+
+        else:
+            if nums[mid] < target < nums[right]:
+                left = mid + 1
+            else:
+                right = mid - 1
+    return index
+
+
+def test():
+    arr = [19, 21, 100, 101, 1, 4, 5, 7, 12]
+    assert broken_search(arr, 5) == 6
+
+    arr = [5, 1]
+    assert broken_search(arr, 1) == 1
+
+    arr = [3, 5, 6, 7, 9, 1, 2]
+    assert broken_search(arr, 4) == -1
+
+    arr = [1, 2, 3, 5, 6, 7, 9, 0]
+    assert broken_search(arr, 3) == 2
+
+    arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 0]
+    assert broken_search(arr, 1) == 0
