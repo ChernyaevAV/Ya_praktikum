b5294eb989dc8a6f6ad3520ac342e7faefe3645f
andrew_cherry <chernyaevav@gmail.com>
2022-12-21 22:53:25 +0300

    B_эффективная быстрая сортировка

---
+++ sprint_13_final/B_effective_quick_sort.py
@@ -0,0 +1,47 @@
+# ID: 79866754
+from random import sample
+
+
+def quicksort(array, start, end):
+    if end - start > 1:
+        p = partition(array, start, end)
+        quicksort(array, start, p)
+        quicksort(array, p + 1, end)
+
+
+def partition(array, left, right):
+    pivot = sample(array[left:right],1)[0]
+    l = left + 1
+    r = right - 1
+
+    while True:
+        while l <= r and array[l] <= pivot:
+            l += 1
+        while l <= r and array[r] >= pivot:
+            r -= 1
+
+        if l <= r:
+            array[l], array[r] = array[r], array[l]
+        else:
+            array[left], array[r] = array[r], array[left]
+            return r
+
+
+def read_data():
+    n = int(input())
+    res = [[-int(completed), int(penalty), name]
+           for name, completed, penalty
+           in [input().strip().split() for _ in range(n)]]
+    return res
+
+
+def main():
+    arr = read_data()
+    quicksort(arr, 0, len(arr))
+
+    for item in arr:
+        print(item[-1])
+
+
+if __name__ == '__main__':
+    main()
