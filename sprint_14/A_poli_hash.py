4893aa0feee030cb7238e8ce670395fded97d0ef
andrew_cherry <chernyaevav@gmail.com>
2022-12-28 22:07:25 +0300

    A_Полиномиальный хеш

---
+++ sprint_14/A_poli_hash.py
@@ -0,0 +1,31 @@
+def horner_hash(base, modul, data_string):
+    res = 0
+    for char in data_string:
+        res = (res * base + ord(char)) % modul
+    return res
+
+
+def itera_hash(base, modul, data_string):
+    res = 0
+    x = 1
+    for char in data_string:
+        res += (ord(char) * x) % modul
+        x *= base
+    return res
+
+
+def read_data():
+    base = int(input())
+    modul = int(input())
+    data_string = input()
+    return base, modul, data_string
+
+
+def main():
+    base, modul, data_string = read_data()
+    print(itera_hash(base, modul, data_string))
+    print(horner_hash(base, modul, data_string))
+
+
+if __name__ == '__main__':
+    main()
