10d5e94ef136fead395b545411bf81043fccc4c8
andrew_cherry <chernyaevav@gmail.com>
2022-12-31 19:03:57 +0300

    рефакторинг

--- sprint_13_final/B_effective_quick_sort.py
+++ sprint_13_final/B_effective_quick_sort.py
@@ -1,22 +1,39 @@
-# ID: 79866754
+# ID: 80195365
+from typing import List
 
 
-def quicksort(array, start, end):
+# class Candidate:
+#     def __init__(self, name, completed, penalty):
+#         self.__name = name
+#         self.__completed = -int(completed)
+#         self.__penalty = int(penalty)
+#
+#     def __convert(self):
+#         return [self.__completed, self.__penalty, self.__name]
+#
+#     def __lt__(self, other):
+#         return self.__convert() < other.__convert()
+#
+#     def __str__(self):
+#         return self.__name
+
+
+def quicksort(array: List[object], start: int, end: int) -> None:
     if end - start > 1:
         p = partition(array, start, end)
         quicksort(array, start, p)
         quicksort(array, p + 1, end)
 
 
-def partition(array, left, right):
+def partition(array: List[object], left: int, right: int) -> int:
     pivot = array[left]
     l = left + 1
     r = right - 1
 
     while True:
-        while l <= r and array[l] <= pivot:
+        while l <= r and array[l] < pivot:
             l += 1
-        while l <= r and array[r] >= pivot:
+        while l <= r and array[r] > pivot:
             r -= 1
 
         if l <= r:
@@ -26,19 +43,18 @@ def partition(array, left, right):
             return r
 
 
-def read_data():
-    n = int(input())
-    res = [(-int(completed), int(penalty), name)
-           for name, completed, penalty
-           in [input().strip().split() for _ in range(n)]]
-    return res
-
-
 def main():
-    arr = read_data()
-    quicksort(arr, 0, len(arr))
+    n = int(input())
+    # candidates = [Candidate(*input().strip().split()) for _ in range(n)]
+    # quicksort(candidates, 0, len(candidates))
+    # print(*candidates, sep='\n')
 
-    for item in arr:
+    candidates = [[-int(completed), int(penalty), name]
+                  for name, completed, penalty
+                  in [input().strip().split() for _ in range(n)]]
+    quicksort(candidates, 0, len(candidates))
+
+    for item in candidates:
         print(item[-1])
 
 
